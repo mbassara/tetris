5f5db62d70faefea709372cae7c437f587d7c0be
Maciek <mbassara@gmail.com>
2013-05-31 02:37:29 +0200

    kwadraty spadają i zatrzymują się na dole planszy

--- tetris.cpp
+++ tetris.cpp
@@ -6,27 +6,54 @@ JanuszG(małpeczka)enter.net.pl
 
 #include <GL/glut.h>
 #include <stdlib.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
 
 // rozmiary bryły obcinania
+const GLdouble board_width = 200.0;
+const GLdouble board_height = 200.0;
 
-
-const GLdouble width = 200.0;
-const GLdouble height = 200.0;
-
-const GLdouble left = -10.0;
-const GLdouble right = width - left;
+const GLdouble board_left = -10.0;
+const GLdouble board_right = board_width + 50;
 const GLdouble bottom = -10.0;
-const GLdouble top = height - bottom;
-const GLdouble near = 50.0;
-const GLdouble far = 70.0;
+const GLdouble top = board_height - bottom;
+const GLdouble near = -1.0;
+const GLdouble far = 1.0;
 
-// współczynnik skalowania
 
-GLfloat scale = 1.0;
+class Block
+{
+public:
+    GLfloat width;
+    GLfloat height;
+    GLfloat pos_x; 
+    GLfloat pos_y;
 
-// kąty obrotu
+    Block(GLfloat width=20.f, GLfloat height=20.f){
+        this->width = width;
+        this->height = height;
+        this->pos_x = board_width / 2 - width;
+        this->pos_y = board_height - height;
+    }
 
-GLfloat translatey = 0.0;
+    void draw(){
+        glBegin(GL_POLYGON);
+            glVertex2f(0.f, 0.f);
+            glVertex2f(0.f, height);
+            glVertex2f(width, height);
+            glVertex2f(width, 0);
+        glEnd();
+    }
+
+    bool isOnTheBottom(){
+        return pos_y == 0;
+    }
+};
+
+vector<Block> blocks;
+Block block;
 
 void DrawFrame(float width, float height)
 {
@@ -65,18 +92,24 @@ void Display()
     
     // macierz modelowania = macierz jednostkowa
     glLoadIdentity();
-
-    // przesunięcie układu współrzędnych obiektu do środka bryły odcinania
-    glTranslatef( 0, 0, -( near + far ) / 2 );
-    
-    // przesunięcie obiektu - ruch myszką
-    // glTranslatef( translatex, translatey, 0.0 );
     
     // kolor krawędzi obiektu
     glColor3f( 0.0, 0.0, 0.0 );
     
-    // rysowanie obiektu
-    DrawFrame(width, height);
+    // rysowanie ramki
+    DrawFrame(board_width, board_height);
+
+    // jeśli ostatnio przesuwany klocek jest na dole to wrzucamy nowy klocek do bufora
+    if(blocks.back().isOnTheBottom())
+        blocks.push_back(Block());
+
+    // rysujemy wszystkie klocki
+    for(int i = 0; i < blocks.size(); i++){
+        glPushMatrix();
+        glTranslatef( blocks[i].pos_x, blocks[i].pos_y, 0.0 );
+        blocks[i].draw();
+        glPopMatrix();
+    }
 
     // skierowanie poleceń do wykonania
     glFlush();
@@ -100,12 +133,12 @@ void Reshape( int width, int height )
     
     // wysokość okna większa od wysokości okna
     if( width < height && width > 0 )
-        glOrtho( left, right, bottom * height / width, top * height / width, near, far );
+        glOrtho( board_left, board_right, bottom * height / width, top * height / width, near, far );
     else
 
     // szerokość okna większa lub równa wysokości okna
     if( width >= height && height > 0 )
-        glOrtho( left * width / height, right * width / height, bottom, top, near, far );
+        glOrtho( board_left * width / height, board_right * width / height, bottom, top, near, far );
     
     // generowanie sceny 3D
     Display();
@@ -123,10 +156,21 @@ void Keyboard( unsigned char key, int x, int y )
 
 void SpecialKeys( int key, int x, int y )
 {
+    // bierzemy ostatni klocek z bufora
+    Block* block = &(blocks.back());
+
+    // jeśli klocek jest na dole to nie przesuwaj
+    if(block->isOnTheBottom())
+        return;
+
     switch( key )
     {
         // kursor w lewo
     case GLUT_KEY_LEFT:
+        if(block->pos_x > block->width)
+            block->pos_x -= block->width;
+        else if(block->pos_x > 0)
+            block->pos_x = 0;
         break;
         
         // kursor w górę
@@ -135,10 +179,18 @@ void SpecialKeys( int key, int x, int y )
         
         // kursor w prawo
     case GLUT_KEY_RIGHT:
+        if(block->pos_x < board_width - block->width - block->width)
+            block->pos_x += block->width;
+        else if(block->pos_x < board_width - block->width)
+            block->pos_x = board_width - block->width;
         break;
         
         // kursor w dół
     case GLUT_KEY_DOWN:
+        if(block->pos_y > 10)
+            block->pos_y -= 10;
+        else if(block->pos_y > 0)
+            block->pos_y = 0;
         break;
     }
     
@@ -148,13 +200,22 @@ void SpecialKeys( int key, int x, int y )
 
 void Animate(int arg)
 {
-    // rotatey += 1;
+    // bierzemy ostatni klocek z bufora
+    Block* block = &(blocks.back());
+
+    if(block->pos_y > 0){
+        block->pos_y -= 1;
+    }
+
     glutPostRedisplay();
     glutTimerFunc(45, Animate, 0);
 }
 
 int main( int argc, char * argv[] )
 {
+    // wrzucamy do bufora klocków pierwszy klocek
+    blocks.push_back(Block());
+
     // inicjalizacja biblioteki GLUT
     glutInit( & argc, argv );
     
